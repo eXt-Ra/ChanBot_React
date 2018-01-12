bd11e4524292d57ba854e16519f859285ae6652a
eXt-Ra <vanardois.romain@gmail.com>
2018-01-12 15:25:17 +0100

    ajout de l'api

---
+++ src/api/query.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * Initer data
+ * @param  {[type]} query [description]
+ * @return {[type]}        [description]
+ */
+export function query(text) {
+  return new Promise((resolve, reject) => {
+    const headers = { Accept: 'application/json', 'Content-Type': 'application/json' }
+    const body = { text };
+    const API_URL = 'http://localhost:5000';
+
+    window.fetch(`${API_URL}`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body)
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      resolve(result)
+    })
+    .catch((err) => {
+      return reject(err);
+    })
+  });
+}
